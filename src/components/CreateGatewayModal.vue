2be3f332b3e83eaca3cd12ee0e1ea002bb9e1c0b
Jose Gabriel Companioni Benitez <gcompanionib@gmail.com>
2022-07-27 23:16:08 -0400

    Added basic validations when creating a Gateway

--- src/components/CreateGatewayModal.vue
+++ src/components/CreateGatewayModal.vue
@@ -2,41 +2,74 @@
   <modal
     :visible="isVisible"
     :mask-closable="false"
-    :confirm-loading="isLoading"
+    :confirm-loading="data.isLoading"
     title="Create Gateway"
     @cancel="emitCloseEvent"
-    @ok="onCreateGateway"
+    @ok="fieldsValidation"
   >
-    <a-input
-      v-model:value="uniqueSerialNumber"
-      placeholder="Serial Number"
-      :allow-clear="true"
-    />
-    <hr />
-    <a-input v-model:value="label" placeholder="Label" :allow-clear="true" />
-    <hr />
-    <a-input
-      v-model:value="address"
-      placeholder="IP Address"
-      :allow-clear="true"
-    />
+    <a-form
+      :model="data"
+      :label-col="{ span: 8 }"
+      :wrapper-col="{ span: 16 }"
+      autocomplete="off"
+      name="form_in_modal"
+      ref="formRef"
+    >
+      <a-form-item
+        label="Serial Number"
+        name="uniqueSerialNumber"
+        :rules="[{ required: true, message: 'Serial Number is required!' }]"
+      >
+        <a-input
+          v-model:value="data.uniqueSerialNumber"
+          placeholder="Serial Number"
+          :allow-clear="true"
+        />
+      </a-form-item>
+      <a-form-item
+        label="Label"
+        name="label"
+        :rules="[{ required: true, message: 'Label is required!' }]"
+      >
+        <a-input
+          v-model:value="data.label"
+          placeholder="Label"
+          :allow-clear="true"
+        />
+      </a-form-item>
+      <a-form-item
+        label="IP Address"
+        name="address"
+        :rules="[{ required: true, message: 'IP Address is required!' }]"
+      >
+        <a-input
+          v-model:value="data.address"
+          placeholder="IP Address"
+          :allow-clear="true"
+        />
+      </a-form-item>
+    </a-form>
   </modal>
 </template>
 
 <script lang="ts">
-import { defineComponent, reactive, toRefs } from "vue";
+import { defineComponent, reactive, ref } from "vue";
 import { CreateGatewayModalType } from "@/types/components";
 import { openNotificationWithIcon, getErrorMessage } from "@/composables/utils";
 import { createGateway } from "@/composables/api";
-import { Modal, Input } from "ant-design-vue";
+import { Modal, Input, Form, FormItem } from "ant-design-vue";
+import type { FormInstance } from "ant-design-vue";
 import "ant-design-vue/lib/modal/style/css";
 import "ant-design-vue/lib/input/style/css";
+import "ant-design-vue/lib/form/style/css";
 
 export default defineComponent({
   name: "CreateGatewayModal",
   components: {
     Modal,
     AInput: Input,
+    AForm: Form,
+    AFormItem: FormItem,
   },
   props: {
     isVisible: {
@@ -45,16 +78,15 @@ export default defineComponent({
     },
   },
   setup(props, ctx) {
-    const data: CreateGatewayModalType = reactive({
+    const formRef = ref<FormInstance>();
+    const data = reactive<CreateGatewayModalType>({
       uniqueSerialNumber: "",
       address: "",
       label: "",
       isLoading: false,
     });
     const emitCloseEvent = () => {
-      data.uniqueSerialNumber = "";
-      data.label = "";
-      data.address = "";
+      formRef.value?.resetFields();
       ctx.emit("close");
     };
     const onCreateGateway = async () => {
@@ -62,9 +94,7 @@ export default defineComponent({
         data.isLoading = true;
         await createGateway(data.uniqueSerialNumber, data.address, data.label);
         openNotificationWithIcon("success", "Gateway succesfully created");
-        data.uniqueSerialNumber = "";
-        data.label = "";
-        data.address = "";
+        formRef.value?.resetFields();
         ctx.emit("success");
       } catch (error) {
         const message = getErrorMessage(error);
@@ -77,11 +107,21 @@ export default defineComponent({
         data.isLoading = false;
       }
     };
+
+    const fieldsValidation = async () => {
+      try {
+        await formRef.value?.validateFields();
+        await onCreateGateway();
+      } catch (error) {
+        console.log("Validation error info: ", error);
+      }
+    };
     return {
       props,
       emitCloseEvent,
-      onCreateGateway,
-      ...toRefs(data),
+      data,
+      fieldsValidation,
+      formRef,
     };
   },
 });
