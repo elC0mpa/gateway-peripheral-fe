f9706b5a78b003c070a09d187f50bec83d229aba
Jose Gabriel Companioni Benitez <gcompanionib@gmail.com>
2022-07-29 12:32:37 -0400

    Solving IPAddress validation issues

--- src/composables/validations.ts
+++ src/composables/validations.ts
@@ -1,5 +1,8 @@
 export const ipAddressValidation = (_: any, value: string) => {
-  if (/^((25[0-5]|(2[0-4]|1\d|[1-9]|)\d)(\.(?!$)|$)){4}$/.test(value)) {
+  if (
+    /^((25[0-5]|(2[0-4]|1\d|[1-9]|)\d)(\.(?!$)|$)){4}$/.test(value) ||
+    !value.length
+  ) {
     return Promise.resolve();
   }
   return Promise.reject(new Error("This is not a valid IP Address"));
